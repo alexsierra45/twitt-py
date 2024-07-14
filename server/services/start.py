aca349bee12bdaa56a41c987d8dd8a079dcf642a
Alex <alexsierraalcala@gmail.com>
2024-07-14 13:02:25 -0400

    fix service

--- server/services/start.py
+++ server/services/start.py
@@ -3,30 +3,6 @@ from persistency.user import UserPersitency
 from services.auth_service import start_auth_service
 from config import NETWORK
 
-
-# func Start(rsaPrivateKeyPath string, rsaPublicteKeyPath string, network string) {
-# 	var err error
-
-# 	rsaPrivate = rsaPrivateKeyPath
-# 	rsaPublic = rsaPublicteKeyPath
-
-# 	node, err = chord.DefaultNode()
-
-# 	if err != nil {
-# 		log.Fatalf("Can't start chord node")
-# 	}
-
-# 	port := "10000"
-# 	broadListen := "11000"
-# 	broadRequest := "12000"
-# 	node.Start(port, broadListen, broadRequest)
-
-# 	go StartUserService(network, "0.0.0.0:50051")
-# 	go StartAuthServer(network, "0.0.0.0:50052")
-# 	go StartPostsService(network, "0.0.0.0:50053")
-# 	go StartFollowService(network, "0.0.0.0:50054")
-# }
-
 def start():
 
     node = ChordNode()
