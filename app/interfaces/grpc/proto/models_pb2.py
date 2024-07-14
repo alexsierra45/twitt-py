76b132c4178033af2b3b594440be3088ce0e49de
Alex <alexsierraalcala@gmail.com>
2024-07-14 02:10:45 -0400

    auth service

--- app/interfaces/grpc/proto/models_pb2.py
+++ app/interfaces/grpc/proto/models_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12proto/models.proto\x12\x07twittpy\"G\n\x04User\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12proto/models.proto\x12\x07twittpy\"L\n\x04User\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x15\n\ruser_password\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\tb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,5 +22,5 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.models_pb2', _globals
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_USER']._serialized_start=31
-  _globals['_USER']._serialized_end=102
+  _globals['_USER']._serialized_end=107
 # @@protoc_insertion_point(module_scope)
