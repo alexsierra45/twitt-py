4ea2a43a0f70f77eea1a5a9b92e0d0720cf84c04
Jky45 <juankyespinosa@gmail.com>
2024-07-15 06:03:48 -0300

    auth service

--- client/grpc_client.py
+++ client/grpc_client.py
@@ -1,4 +1,6 @@
+from venv import logger
 import grpc
+from grpclib import GRPCError
 from proto import auth_pb2, auth_pb2_grpc, models_pb2, models_pb2_grpc
 
 channel = grpc.insecure_channel('localhost:5000')
@@ -7,8 +9,14 @@ user_stub = auth_pb2_grpc.AuthStub(channel)
 def sign_up(email, username, name, password):
     user = models_pb2.User(email=email, username=username, name=name, password=password)
     request = auth_pb2.SignUpRequest(user=user)
-    response = user_stub.SignUp(request)
-    print (response)
+    try:
+        response = user_stub.SignUp(request)
+        print (response)
+        return True
+    except grpc.InactiveRpcError as error:
+        print("holaaaaaaaa")
+        # print(error)
+        logger.error(f"An error occurred creating the user: {error.status}: {error.details}")
 
 
 def login(username, password):
@@ -19,3 +27,7 @@ def login(username, password):
     response = user_stub.Login(request)
     print (response)
     return True
+
+
+
+
