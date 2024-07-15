8d9ff8204f164cc65a93ecd05bde9951251b4e4b
Jky45 <juankyespinosa@gmail.com>
2024-07-15 03:34:08 -0300

    updating

--- client/grpc_client.py
+++ client/grpc_client.py
@@ -1,8 +1,8 @@
 import grpc
 from proto import auth_pb2, auth_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:50051')
-user_stub = auth_pb2_grpc.UserServiceStub(channel)
+channel = grpc.insecure_channel('172.31.141.188:5000')
+user_stub = auth_pb2_grpc.AuthServiceStub(channel)
 
 def sign_up(email, username, name, password):
     return user_stub.SignUp(auth_pb2.SignUpRequest(email=email, username=username, name=name, password=password))
